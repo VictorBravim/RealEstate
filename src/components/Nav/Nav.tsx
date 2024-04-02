aabcc59bbb0ca12dcc245384700ff6d203b11f0c
Victor Bravim <victorrafaelbravim@gmail.com>
2024-04-02 11:07:27 -0300

    Mudança

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,18 +1,24 @@
 // Nav.tsx
 'use client'
-import { useState } from 'react'
+import { useState } from 'react';
 import Image from 'next/image';
 import "bootstrap/dist/css/bootstrap.min.css"; 
-import "./Nav.css"
-import logo from '@/assets/logo.png'
+import "./Nav.css";
+import logo from '@/assets/logo.png';
 
 export default function Nav() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
         <div>
-            <div className="site-mobile-menu site-navbar-target">
+            <div className={`site-mobile-menu site-navbar-target ${isMenuOpen ? 'open' : ''}`}>
                 <div className="site-mobile-menu-header">
                     <div className="site-mobile-menu-close">
-                        <span className="icofont-close js-menu-toggle"></span>
+                        <span className="icofont-close js-menu-toggle" onClick={toggleMenu}></span>
                     </div>
                 </div>
                 <div className="site-mobile-menu-body"></div>
@@ -26,7 +32,7 @@ export default function Nav() {
                                 <Image src={logo} alt="Logo" width={50} height={40} />Casa Nova
                             </a>
 
-                            <ul className="js-clone-nav d-none d-lg-inline-block text-start site-menu float-end">
+                            <ul className={`js-clone-nav d-none d-lg-inline-block text-start site-menu float-end ${isMenuOpen ? 'd-block' : 'd-none'}`}>
                                 <li className="active"><a href="index.html">Home</a></li>
                                 <li><a href="/propriedade.html">Propriedades</a></li>
                                 <li><a href="/serviços.html">Serviços</a></li>
@@ -35,7 +41,7 @@ export default function Nav() {
 
                             <a href="#"
                                 className="burger light me-auto float-end mt-1 site-menu-toggle js-menu-toggle d-inline-block d-lg-none"
-                                data-toggle="collapse" data-target="#main-navbar">
+                                onClick={toggleMenu}>
                                 <span></span>
                             </a>
                         </div>
