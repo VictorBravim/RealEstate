7b72ea8fb13862d38df9d5e29f935e786bbbc510
Victor Bravim <victorrafaelbravim@gmail.com>
2024-05-21 16:05:38 -0300

    Mudança

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,8 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Casa Nova",
-  description: "Imobiliaria Casa Nova",
+  title: "RealEstate",
+  description: "Imobiliaria",
 };
 
 export default function RootLayout({
