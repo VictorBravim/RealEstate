f6ad8d1e2a4295dd91405229eeac0192260e32da
Victor Bravim <victorrafaelbravim@gmail.com>
2024-05-22 11:31:45 -0300

    Mudança

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -7,28 +7,18 @@ import logo from '@/assets/logo.png';
 
 export default function Footer() {
     return (
-        <footer className="site-footer">
+        <footer className="site-footer bg-black text-white">
             <div className="container">
-                <div className="row">
-                    <div className="col-lg-4">
+                <div className="row justify-content-between">
+                    <div className="col-lg-4 col-md-6 mb-4 mb-lg-0 d-flex justify-content-start">
                         <Link href="/">
-                            <li className="logo">
-                                <Image src={logo} alt="Logo" width={50} height={40} className='mr-6' />
-                            </li>
+                            <div className="logo">
+                                <Image src={logo} alt="Logo" width={50} height={40} />
+                            </div>
                         </Link>
                     </div>
-                    <div className="col-lg-8">
-                        <div className="row">
-                            <div className="col-md-6 col-lg-4">
-                                <h2>Links</h2>
-                            </div>
-                            <div className="col-md-6 col-lg-4">
-                                <h2>Outros Links</h2>
-                            </div>
-                            <div className="col-md-6 col-lg-4">
-                                <h2>Redes Sociais</h2>
-                            </div>
-                        </div>
+                    <div className="col-lg-3 col-md-6 mb-4 mb-lg-0 text-center">
+                        <p className="text-muted mb-0">&copy; {new Date().getFullYear()} Your Company Name</p>
                     </div>
                 </div>
             </div>
